b69338f88ae8e337f6c2394711a660cc31bc8a95
2dawa <2dawa@informatica.edu>
2024-12-04 10:35:15 +0000

    Add UT04-02

--- DEW/UT04-02/script.js
+++ DEW/UT04-02/script.js
@@ -9,6 +9,9 @@ const DOM = {
     descriptionCount: document.getElementById("postdata-count_description"),
     form: document.getElementById("form"),
     username: document.getElementById("userdata_username"),
+    hobbiesInput: document.getElementById("hobbies"),
+    hobbiesList: document.querySelectorAll(".aficiones input[type=checkbox]"),
+
 }
 
 // Mostrar contraseña
@@ -31,6 +34,20 @@ DOM.description.addEventListener("input", (e) => {
     DOM.descriptionCount.textContent = `${e.target.value.length}/120`; 
 })
 
+// DOM.form.addEventListener("submit", (e) => {
+//     DOM.inputList.foreach(input => {
+//         console.log(input.validationMessage);
+//         if (!input.validationMessage == ""){
+//             e.preventDefault();
+//             let span = document.createElement("span");
+//             span.textContent = input.validationMessage;
+//             span.classList.toggle("errormessage");
+            
+//             input.after(span);
+//         }
+//     });
+// })
+
 DOM.form.addEventListener("submit", (e) => {
     if (!DOM.username.validationMessage == ""){
         e.preventDefault();
@@ -40,4 +57,19 @@ DOM.form.addEventListener("submit", (e) => {
         
         DOM.username.after(span);
     }
+})
+
+DOM.form.addEventListener("submit", (e) => {
+    let hobbies = [];
+    DOM.hobbiesList.forEach(hobby => {
+        if (hobby.checked) {
+            hobbies.push(hobby.value)
+        }
+        console.log(hobbies);
+    });
+
+    console.log(DOM.hobbiesList.filter(hobby => hobby.checked))
+    console.log(DOM.hobbiesInput.value);
+    DOM.hobbiesInput.value = hobbies.length >= 2 ? hobbies.toString() : null;
+    console.log(DOM.hobbiesInput.value);
 })
