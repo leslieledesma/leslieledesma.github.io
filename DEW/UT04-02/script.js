673eb34de160b8652fac43a8a8f4650a6d7b3d33
Leslie <ledesma.leslie@gmail.com>
2024-12-06 17:39:29 +0000

    Complete UT04-02

--- DEW/UT04-02/script.js
+++ DEW/UT04-02/script.js
@@ -1,30 +1,78 @@
 const DOM = {
-    username: document.getElementById("userdata_username"),
-    password: document.getElementById("userdata_password"),
+    input: {
+        username: document.getElementById("userdata_username"),
+        password: document.getElementById("userdata_password"),
+        name: document.getElementById("personaldata_name"),
+        surname: document.getElementById("personaldata_surname"),
+        phone: document.getElementById("personaldata_telephone"),
+        postalCode: document.getElementById("personaldata_cp"),
+        documentValue: document.getElementById("personaldata-input_dninie"),
+        hobbies: document.getElementById("personaldata_hobbies"),
+        title: document.getElementById("postdata_title"),
+        description: document.getElementById("postdata_description"),
+    },
+    select: {
+        documentType: document.getElementById("personaldata-select_dninie"),
+        birthyear: document.getElementById("personaldata_birthyear"),
+        accountType: document.getElementById("accounttype_particular"),
+    },
+    
     showPassword: document.getElementById("userdata_showpassword"),
-
-    name: document.getElementById("personaldata_name"),
-    surname: document.getElementById("personaldata_surname"),
-    phone: document.getElementById("personaldata_telephone"),
-    postalCode: document.getElementById("personaldata_cp"),
-
-    selectDNINIE: document.getElementById("personaldata-select_dninie"),
-    inputDNINIE: document.getElementById("personaldata-input_dninie"),
-    accounttype: document.getElementsByName("CuentaComo"),
-    birthyearSelect: document.getElementById("personaldata_birthyear"),
-
-    hobbiesInput: document.getElementById("hobbies"),
+    accountType: document.querySelectorAll("input[name=CuentaComo]"),
     hobbiesList: document.querySelectorAll(".aficiones input[type=checkbox]"),
-
-    title: document.getElementById("postdata_title"),
-    description: document.getElementById("postdata_description"),
     titleCount: document.getElementById("postdata-count_title"),
     descriptionCount: document.getElementById("postdata-count_description"),
-
     form: document.getElementById("form"),
     inputList: document.querySelectorAll("input[type=text]"),
-
     erroresSection: document.getElementById("errores"),
+}
+
+const errorDisplay = {
+    username: document.getElementById("userdata-error_username"),
+    password: document.getElementById("userdata-error_password"),
+    name: document.getElementById("personaldata-error_name"),
+    surname: document.getElementById("personaldata-error_surname"),
+    phone: document.getElementById("personaldata-error_telephone"),
+    postalCode: document.getElementById("personaldata-error_cp"),
+    documentType: document.getElementById("personaldata-select-error_doctype"),
+    documentValue: document.getElementById("personaldata-input-error_doctype"),
+    accountType: document.getElementById("personaldata-error_accounttype"),
+    birthyear: document.getElementById("personaldata-error_birthyear"),
+    hobbies: document.getElementById("personaldata-error_hobbies"),
+    title: document.getElementById("postdata-error_title"),
+    description: document.getElementById("postdata-error_description"),
+}
+
+const errorMessageRequired = {
+    username: "El nombre de usuario es obligatorio",
+    password: "La contraseña es obligatoria",
+    name: "El nombre es obligatorio",
+    surname: "El apellido es obligatorio",
+    phone: "El número de teléfono es obligatorio",
+    postalCode: "El código postal es obligatorio",
+    documentType: "Debes seleccionar un tipo de documento",
+    documentValue: "El documento es obligatorio",
+    accountType: "Debes seleccionar un tipo de cuenta",
+    birthyear: "Debes seleccionar un año de nacimiento",
+    hobbies: "Debes seleccionar al menos 2 aficiones",
+    title: "El título de la publicación es obligatorio",
+    description: "La descripción de la publicación es obligatoria",
+}
+
+const errorMessageFormat = {
+    username: "El nombre de usuario debe tener entre 4 y 20 caracteres",
+    password: "La contraseña debe estar compuesta por 8 dígitos",
+    name: "El nombre debe tener entre 4 y 20 caracteres",
+    surname: "El apellido debe tener entre 4 y 20 caracteres",
+    phone: "El número telefónico debe incluir el código de la zona entre paréntesis y 9 dígitos",
+    postalCode: "El código postal debe comenzar por 38 y contener 5 dígitos",
+    documentType: "",
+    documentValue: "El documento debe cumplir con el formato requerido",
+    accountType: "Debes seleccionar un tipo de cuenta",
+    birthyear: "",
+    hobbies: "",
+    title: "El título de la publicación debe tener entre 4 y 15 caracteres",
+    description: "La descripción de la publicación debe tener entre 4 y 120 caracteres",
 }
 
 // Generar las opciones para el año de nacimiento
@@ -34,56 +82,25 @@ for (let year = startYear; year <= endYear; year++) {
     let option = document.createElement('option');
     option.value = year;
     option.textContent = year;
-    DOM.birthyearSelect.appendChild(option);
+    DOM.select.birthyear.appendChild(option);
 }
 
 // Mostrar/ocultar contraseña
 DOM.showPassword.addEventListener("change", () => {
-    DOM.password.type = DOM.showPassword.checked ? "text" : "password";
+    DOM.input.password.type = DOM.showPassword.checked ? "text" : "password";
 });
 
 // Deshabilitar uso de letras en el input de Código Postal
-DOM.postalCode.addEventListener("keydown", (e) => {
+DOM.input.postalCode.addEventListener("keydown", (e) => {
     if (/^[a-zA-Z\s]$/.test(e.key)) {
         e.preventDefault();
     }
 });
 
 // Habilitar/deshabilitar input del tipo de documento y establecer placeholder
-DOM.selectDNINIE.addEventListener("change", (e) => {
-    DOM.inputDNINIE.disabled = e.target.value ? false : true;
-    DOM.inputDNINIE.setAttribute("placeholder", `${e.target.value == "DNI" ? "Ej: 12345678A" : "Ej: X1234567A"}`)
-});
-
-// TODO: Validación del tipo de documento
-DOM.selectDNINIE.addEventListener("change", (e) => {
-        // .setCustomValidity("Mensaje");
-        esDniNieValido(e.target.value)
-
-    function esDniNieValido(value) {
-        value = value.toUpperCase().trim();
-        
-        // Patrones para DNI y NIE
-        const dniPattern = /^\d{8}[A-HJ-NP-TV-Z]$/;
-        const niePattern = /^[XYZ]\d{7}[A-HJ-NP-TV-Z]$/;
-    
-        if (!dniPattern.test(value) && !niePattern.test(value)) {
-            return false; // No coincide con el formato esperado
-        }
-    
-        // Si es un NIE, sustituimos la letra inicial por su correspondiente número
-        if (niePattern.test(value)) {
-            const niePrefix = { X: "0", Y: "1", Z: "2" };
-            value = value.replace(/^[XYZ]/, letra => niePrefix[letra]);
-        }
-    
-        const numero = parseInt(value.slice(0, 8), 10);
-        const letra = value.slice(-1);
-    
-        const letrasValidas = "TRWAGMYFPDXBNJZSQVHLCKE";
-        const letraEsperada = letrasValidas[numero % 23];
-        return letra === letraEsperada;
-    }
+DOM.select.documentType.addEventListener("change", (e) => {
+    DOM.input.documentValue.disabled = e.target.value ? false : true;
+    DOM.input.documentValue.setAttribute("placeholder", `${e.target.value == "DNI" ? "Ej: 12345678A" : "Ej: X1234567A"}`)
 });
 
 // Comprobar y dar valor al input de aficiones
@@ -91,50 +108,91 @@ DOM.hobbiesList.forEach(hobby => {
     hobby.addEventListener("change", () => {
         let hobbiesList = [...DOM.hobbiesList];
         let checkedHobbies = hobbiesList.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
-        DOM.hobbiesInput.value = checkedHobbies.length >= 2 ? checkedHobbies.join(",") : "";
+        DOM.input.hobbies.value = checkedHobbies.length >= 2 ? checkedHobbies.join(",") : "";
+        if (checkedHobbies.length >= 2) errorDisplay.hobbies.textContent = "";
     })
 });
 
 // Contar caracteres de título
-DOM.title.addEventListener("input", (e) => {
+DOM.input.title.addEventListener("input", (e) => {
     DOM.titleCount.textContent = `${e.target.value.length}/15`; 
 })
 
 // Contar caracteres de descripción
-DOM.description.addEventListener("input", (e) => {
+DOM.input.description.addEventListener("input", (e) => {
     DOM.descriptionCount.textContent = `${e.target.value.length}/120`; 
 })
 
-
-
-// Validación de errores
-let checkValidationInput = [DOM.username, DOM.password, DOM.name, DOM.surname, DOM.phone, DOM.postalCode, DOM.inputDNINIE, DOM.birthyearSelect, DOM.hobbiesInput, DOM.title, DOM.description];
-// TODO: quitar el mensaje de los checkbox e input radio
-checkValidationInput.forEach(element => {
-    element.addEventListener("input", (e) => {
-        if (e.target.validationMessage == ""){
-            e.target.nextElementSibling.textContent = "";
-        }
-    })
-});
+// Validación de elementos al hacer submit
 DOM.form.addEventListener("submit", (e) => {
     DOM.form.setAttribute("class", "submitted");
 
-    // TODO: validar input radio
-    checkValidationInput.forEach(element => {
-        if (!element.validationMessage == ""){
-            e.preventDefault();
-            let span = element.nextElementSibling;
-            span.textContent = element.validationMessage;
-            span.setAttribute("class", "errormessage");
-        }
-    })
-    if (!DOM.selectDNINIE.validationMessage == ""){
-        e.preventDefault();
-        let span = document.getElementById("personaldata-error_doctype");
-        span.textContent = DOM.selectDNINIE.validationMessage;
-        span.setAttribute("class", "errormessage");
-    }
-})
+    // Limpiar el registro de errores de la sección derecha si tiene elementos
+    let spans = DOM.erroresSection.querySelectorAll("span");
+    if (spans.length != 0) spans.forEach(span => DOM.erroresSection.removeChild(span));
 
-// TODO: Agregar los validationMessage en el section de la derecha
+    let hasErrors = validateFields(DOM.input, "campo") | validateFields(DOM.select, "lista") | !isDocumentValid();
+    
+    if(hasErrors) {
+        e.preventDefault();
+        checkDocument();
+        validateCorrections(DOM.input, "input");
+        validateCorrections(DOM.select, "change");
+        validateCorrectionsInputRadio(DOM.accountType);
+    }
+});
+
+// Funciones auxiliares
+function validateFields(list, term) {
+    let hasErrors = false;
+    for (const key in list) {
+        if (list[key].validationMessage) {
+            hasErrors = true;
+            errorDisplay[key].textContent = 
+                list[key].validationMessage.includes(term) ? errorMessageRequired[key] : errorMessageFormat[key];
+            let span = document.createElement("span");
+            span.textContent = `${list[key].getAttribute("name")}: ${list[key].validationMessage}`
+            DOM.erroresSection.appendChild(span);
+        }
+        else errorDisplay[key].textContent = "";
+    }
+    return hasErrors;
+}
+function validateCorrections(list, event) {
+    Object.entries(list).forEach(([key, element]) => {
+        element.addEventListener(event, (e) => {
+            if (e.target.validationMessage == ""){
+                errorDisplay[key].textContent = "";
+            }
+        })
+    });
+}
+function validateCorrectionsInputRadio(optionList) {
+    optionList.forEach(option =>
+        option.addEventListener("change", (e) => {
+            if (e.target.validationMessage == ""){
+                errorDisplay.accountType.textContent = "";
+            }
+        })
+    );
+}
+// Para validar el documento
+function isDocumentValid() {
+    let docValue = DOM.input.documentValue.value.trim();
+
+    // Reemplaza las letras X, Y, Z por 0, 1, 2 si es un NIE
+    if (DOM.select.documentType.value == "NIE") {
+        docValue = docValue.replace(/X/g, '0').replace(/Y/g, '1').replace(/Z/g, '2');;
+    }
+
+    const numero = parseInt(docValue.substring(0, docValue.length - 1));
+    const letra = docValue.charAt(docValue.length - 1).toUpperCase();
+    const letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
+
+    return letra == letras[numero % 23];
+}
+function checkDocument() {
+    if (!DOM.input.documentValue.disabled && DOM.input.documentValue.value != "") {
+        errorDisplay.documentValue.textContent = !isDocumentValid() ? "El documento no es válido" : "";
+    }
+}
